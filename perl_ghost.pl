9b3018f51aca349404267732fc9f0226e5cb7429
gibusu1 <zhutianzhong066@gmail.com>
2026-07-05 05:05:21 +0900

    Create perl_ghost.pl

---
+++ perl_ghost.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+print "💀 [Perl/Ghost] 影の始末屋、オンライン...\n";
+print "💀 [Perl/Ghost] 3人のログを暗号化の霧でカモフラージュ中。\n";
+
+my $threat_detected = 0; # 敵の気配（0=平穏, 1=接近）
+
+if ($threat_detected == 1) {
+    print "💀 [Perl/Ghost] ハッカーの逆探知パケットを検知。文字通り『バラバラ』に解体しました。\n";
+} else {
+    print "💀 [Perl/Ghost] アニキ、背後は僕が消しておいたよ。ふふ...\n";
+}
