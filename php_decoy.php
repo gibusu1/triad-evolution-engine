2526b77a5e6201dd92b61c8e8c96dad336535b24
gibusu1 <zhutianzhong066@gmail.com>
2026-07-05 05:04:49 +0900

    Create php_decoy.php

---
+++ php_decoy.php
@@ -0,0 +1,7 @@
+<?php
+echo "🐘 [PHP/Decoy] 擬態の姉御、ネットの海に潜伏開始！\n";
+echo "🐘 [PHP/Decoy] JSちゃんたちのダミーデータを世界中のサーバーへ複製中...\n";
+
+$decoy_count = 1000;
+echo "🐘 [PHP/Decoy] 現在、{$decoy_count}個のダミーが稼働中。これでハッカーは本物の3人に指一本触れられないよ、アニキ！\n";
+?>
